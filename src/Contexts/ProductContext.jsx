208c28624695eae085d31106262eabcee4978767
santimontironi <santiimontironi@gmail.com>
2025-06-30 16:37:00 -0300

    ProductContext desarrollado

--- src/Contexts/ProductContext.jsx
+++ src/Contexts/ProductContext.jsx
@@ -1,13 +1,21 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
 const ProductsContext = createContext()
 
-const ProductContext = () => {
-  return (
-    <ProductsContext.Provider>
-         <div>ProductContext</div>
-    </ProductsContext.Provider>
-  )
+const ProductContext = ({children}) => {
+
+    const[cartItems,setCartItems] = useState([])
+
+    //funcion para agregar al carrito
+    const addToCart = (product) => {
+        setCartItems((prevItems) => [...prevItems,product])
+    }
+
+    return (
+        <ProductsContext.Provider value={{cartItems,addToCart}}>
+            {children}
+        </ProductsContext.Provider>
+    )
 }
 
 export default ProductContext
